f58ea25e7c09a4b7d0cd5be4a47fb51a8243ef2c
albertchen857 <104204390+albertchen857@users.noreply.github.com>
2022-04-22 20:12:49 +0800

    Update and rename 分数.py to ScoreCalculator_En.py
    used uasygui

--- ScoreCalculator_En.py
+++ ScoreCalculator_En.py
@@ -8,21 +8,21 @@ c1=0
 c2=0
 c3=0
 #a1/a2 +-*/ b1/b2 = sum1/sum2
-easygui.msgbox("分数计算器",title="人性化计算器")
-n1=input("分数计算器·ENTER进入教程")
+easygui.msgbox("Score calculator",title="great caculator")
+n1=input("Score calculator·Click ENTER to enter the tutorial ")
 n1=input('''====================
-输入1:分数1分子
-输入2:分数1分母
-输入3:运算符号[+加 -减 *乘 /除]
-输入4:分数2分子
-输入5:分数2分母
-ENTER开始使用·好用记得三连''')
+input1:Fraction1 molecular
+input2:Fraction1 denominator
+input3:calculating signs[+plus -minus *Multiplication /division]
+input4:Fraction2 molecular
+input5:Fraction2 denominator
+Click ENTER go ahead''')
 while True:
-    a1=int(input("分数1分子:"))
-    a2=int(input("分数1分母:"))
-    a=input("运算符号[+-*/]:")
-    b1=int(input("分数2分子:"))
-    b2=int(input("分数2分母:"))
+    a1=int(input("Fraction1 molecular:"))
+    a2=int(input("Fraction1 denominator:"))
+    a=input("calculating signs[+plus -minus *Multiplication /division]:")
+    b1=int(input("Fraction2 molecular:"))
+    b2=int(input("Fraction2 denominator:"))
     if a=="+":
         c1=a2
         c2=b2
@@ -36,8 +36,7 @@ while True:
         sum1=c1/c3
         sum2=c2/c3
         print(a1,"/",a2,"+",b1,"/",b2,"=",sum1,"/",sum2)
-    n2=input("是否退出(Y-N/ENTER)")
-    if a=="-":
+    elif a=="-":
         c1=a2
         c2=b2
         a2=a2*c2
@@ -50,21 +49,20 @@ while True:
         sum1=c1/c3
         sum2=c2/c3
         print(a1,"/",a2,"-",b1,"/",b2,"=",sum1,"/",sum2)
-    n2=input("是否退出(Y-N/ENTER)")
-    if a=="*":
+    elif a=="*":
         c1=a1*b1
         c2=a2*b2
         c3=gcd(c1,c2)
         sum1=c1/c3
         sum2=c2/c3
         print(a1,"/",a2,"x",b1,"/",b2,"=",sum1,"/",sum2)
-    if a=="/":
+    elif a=="/":
         c1=a1*b2
         c2=a2*b1
         c3=gcd(c1,c2)
         sum1=c1/c3
         sum2=c2/c3
         print(a1,"/",a2,"÷",b1,"/",b2,"=",sum1,"/",sum2)
-    n2=input("是否退出(Y-N/ENTER)")
+    n2=input("whether to quit(Y-N/ENTER)")
     if n2=="Y":
         break
